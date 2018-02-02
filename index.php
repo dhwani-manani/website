9de99ba5ccde15c5bfb2ead5886b0ef71b9f7e9c
dhwani-manani <36025856+dhwani-manani@users.noreply.github.com>
2018-02-02 15:09:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 session_start();
 if(isset($_POST['bttLogin'])){
     require 'connect.php';
-    $username = $_POST['name'];
+    $name = $_POST['name'];
     $password = $_POST['password'];
-    $result = mysqli_query($con, 'select * from registration where name="'.$username.'"and password="'.$password.'"');
+    $result = mysqli_query($con, 'select * from registration where name="'.$name.'"and password="'.$password.'"');
    if( mysqli_num_rows($result)==1){
-        $_SESSION['username'] = $username;
+        $_SESSION['name'] = $name;
         header('Location: Category.php');
     }
     else
@@ -14,7 +14,7 @@ if(isset($_POST['bttLogin'])){
 }
 
 if(isset($_GET['logout'])){
-	session_unregister('	name');
+	session_unregister('name');
 }
 ?>
 <html>
@@ -24,12 +24,28 @@ if(isset($_GET['logout'])){
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script type="text/javascript" src="script.js"></script>
 <style>
+body{
+	background-image: url("signinimg (2).jpg");
+	background-color:palegoldenrod;
+     background-image: url(1stpgimg.jpg);
+	background-repeat:repeat ; 
+	background-attachment:fixed;
+	color:black;
+}
+     div.container{
+       padding: 100px 450px 25px; 
+    }
+    
+
 ul {
-list-style-type: none;
-margin: 0;
-padding: 0;
-overflow: hidden;
-background-color: #333;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+    position: fixed;
+    top: 0;
+    width: 5000px;
 }
 
 li {
@@ -44,34 +60,46 @@ li a {
     text-decoration: none;
 }
 
-li a:hover {
+li a:hover:not(.active) {
     background-color: #111;
 }
+
+.active {
+    background-color: #4CAF50;
+}
+    
+    label{
+color: #464646;
+text-shadow: 0 1px 0 #fff;
+font-size: 14px;
+font-weight: bold;
+}
+
 </style>
 </head>
-<header>
-<h1>All Books..One Site.!</h1>
-</header>
 <body>
-<nav>
-<ul>  
+<ul>
 <li><a href="ContactUs.html" title="Contact Us">Contact Us</a> </li> 
-<li><a href="Help.html" title="Help">Help</a></h2></li>
+<li><a href="Submit1.html" title="About_us">About_us</a></li>
+<li><a href="Help.html" title="Help">Help</a></li> 
+     <li><a href="First_Page.html" title="Home">Home</a></li>
 </ul>
-</div>
- <center>
-<h1>Login Form</h1>
-<form name = "form" method="post" action="Category.html">
-<h2>User Name:<br>
-<input type = "text" name = "name" ></h2>
-<h2>Password:<br>
-<input type="Password" name="password" ></h2>
-<br>
-<br>
-<input type="submit" name="bttLogin" value="Login" onclick="Category.php">
 
-</center>
+<div class="container" >
+<div class="main">
+<h2>Login Form</h2>
+<form name = "form" method="post" action="Category.php">
+<label>User Name:</label>
+<input type = "text" name = "name" >
+    <br>
+<label>Password :</label>
+<input type="Password" name="password" >
+    <br>
+
+<input type="submit" name="bttLogin" value="Login" id="register" onclick="Category.php">
 </form>
+    </div>
+    </div>
+   
 </body>
 </html>
-
